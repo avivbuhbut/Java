45507896f123c60c971c6c056a420010def2605c
avivbuhbut <avivb931@gmail.com>
2019-12-21 23:22:32 +0200

    working on order method

--- Stock.java
+++ Stock.java
@@ -1,5 +1,6 @@
 import com.sun.istack.internal.FragmentContentHandler;
 import com.sun.jmx.remote.util.OrderClassLoaders;
+import com.sun.org.apache.bcel.internal.generic.DASTORE;
 import com.sun.org.apache.bcel.internal.generic.IUSHR;
 import com.sun.org.apache.xml.internal.security.algorithms.implementations.IntegrityHmac;
 import com.sun.xml.internal.bind.v2.runtime.InlineBinaryTransducer;
@@ -29,7 +30,6 @@ public class Stock {
 	 */
 	private boolean checkIfItemExist(FoodItem newitem, int i) {
 
-
 		if (_stock[i].getName().equals(newitem.getName())
 				&& _stock[i].getCatalogueNumber() == newitem.getCatalogueNumber()
 				&& _stock[i].getExpiryDate().equals(newitem.getExpiryDate())
@@ -43,11 +43,9 @@ public class Stock {
 
 	public boolean addItem(FoodItem newitem) {
 
-
 		int k = 0;
 
 		for (int i = 0; i < _noOfItems; i++) {
-
 
 			if (checkIfItemExist(newitem, i)) {
 
@@ -69,21 +67,19 @@ public class Stock {
 		}
 
 		if (findIndexByName(newitem.getName()) == -1) {
-	
+
 			_stock[k] = new FoodItem(newitem);
 			_noOfItems++;
 		} else {
-		
+
 			int index = findIndexByName(newitem.getName());
 			this.moveForward(index);
 			_stock[index] = newitem;
-	
+
 			k = index;
 			_noOfItems++;
-			System.out.println("in Same Name. addItem");
+
 		}
-
-
 
 		return true;
 
@@ -117,92 +113,46 @@ public class Stock {
 		// other wise add it
 
 		String order = "";
-		for(int i=0; i<_noOfItems;i++) {
+		for (int i = 0; i < _noOfItems; i++) {
 			int totalQuantity = 0;
-			if (amountOfItem(i)>1) {
+			if (amountOfItem(i) > 1) {
 				System.out.println("in");
-				for(int j=i;j<i+amountOfItem(i);j++) { // iterating range of items of same kind
+				for (int j = i; j < i + amountOfItem(i); j++) { // iterating range of items of same kind
 					System.out.println(_stock[j].getName());
-					totalQuantity+=_stock[j].getQuantity();	//adding each items quantity to a total one			
+					totalQuantity += _stock[j].getQuantity(); // adding each items quantity to a total one
 				}
-			}else //only one item of this kind
-				totalQuantity+=_stock[i].getQuantity();
-			
-			if(totalQuantity<amount && i!= _noOfItems-1)
-				order += _stock[i].getName()+ ",";
+			} else // only one item of this kind
+				totalQuantity += _stock[i].getQuantity();
+
+			if (totalQuantity < amount && i != _noOfItems - 1)
+				order += _stock[i].getName() + ",";
 			else
 				order += _stock[i].getName();
-			
-			i+=	amountOfItem(i); //increasing i to be after range
-			
+
+			i += amountOfItem(i); // increasing i to be after range
+
 		}
-						
+
 		return order;
 
-		// checking for duplication
-		//return getList(amount, order);
-
 	}
-	
+
 	/**
 	 * returns how many items of the same kind(same name)
+	 * 
 	 * @param index
 	 * @return
 	 */
 	private int amountOfItem(int index) {
-		
+
 		int equalItems = 0;
-		
-		for(int i = index; i<_noOfItems-1;i++) {
-			if(_stock[i].getName().equals(_stock[i+1].getName()))	
+
+		for (int i = index; i < _noOfItems - 1; i++) {
+			if (_stock[i].getName().equals(_stock[i + 1].getName()))
 				equalItems++;
 		}
-		
-		
-		
+
 		return equalItems;
-	}
-	
-	
-
-	private String getList(int amount, String order) {
-		int k = 0;
-		int sameProductsQuantity;
-
-		for (int j = 0; j < _noOfItems - 1; j++) {
-			sameProductsQuantity = _stock[j].getQuantity();
-
-			if (_stock[j].getName().equals(_stock[j + 1].getName())
-					&& (_stock[j].getQuantity() + _stock[j + 1].getQuantity()) < amount) { // if there are a few items
-		
-
-				k = j + 1;
-				sameProductsQuantity = _stock[k].getQuantity();
-
-				if (_stock[k].getName().equals(_stock[k + 1].getName())) {
-					while (k < _noOfItems && _stock[k].getName().equals(_stock[k + 1].getName())) {
-
-						if (_stock[k].getName().equals(_stock[k + 1].getName())
-								&& (sameProductsQuantity + _stock[k + 1].getQuantity()) < amount)
-							sameProductsQuantity += _stock[k + 1].getQuantity();
-						// System.out.println(k);
-						k++;
-
-					}
-
-				} else {
-					// if (_stock[j].getQuantity() < amount)
-					if (sameProductsQuantity < amount)
-						order += _stock[k].getName() + ",";
-				}
-
-			}
-
-			if (sameProductsQuantity < amount)
-				order += _stock[j].getName() + ",";
-
-		}
-		return order;
 	}
 
 	public int howMany(int temp) {
@@ -216,18 +166,17 @@ public class Stock {
 		return numOfItmToTranfer;
 	}
 
-
 	public void removeAfterDate(Date d) {
 
 		for (int i = 0; i < _noOfItems;) {
 			if (_stock[i].getExpiryDate().before(d)) {
 				for (int j = i; j < _noOfItems - 1; j++) {
-					_stock[j] = _stock[j + 1];				
+					_stock[j] = _stock[j + 1];
 				}
 				_stock[_noOfItems - 1] = null;
-				_noOfItems--;				
-			}else
-			 i++;
+				_noOfItems--;
+			} else
+				i++;
 
 		}
 
@@ -239,8 +188,6 @@ public class Stock {
 		int i = 0;
 		if (_noOfItems < 1)
 			return "out of stock";
-
-
 
 		for (; i < _noOfItems - 1; i++) {
 			System.out.println(_stock[i].toString());
@@ -271,8 +218,25 @@ public class Stock {
 			return 0;
 	}
 
+	public void updateStock(String[] itemsList) {
+
+		// itretite over the array and find how many do i need to reduce from each item
+		for (int j = 0; j < _noOfItems; j++) {
+			for (int i = 0; i < itemsList.length; i++) {
+				if (_stock[j].getName().equals(itemsList[i])) {
+					System.out.println(_stock[j].getName() +" equals "+ itemsList[i]);
+					_stock[j].setQuantity(_stock[i].getQuantity() - 1);
+					
+				}
+			
+			}
+
+		}
+
+	}
+
 	public static void main(String[] args) {
-		
+
 		Stock stock = new Stock();
 		Date productionDate1 = new Date(8, 3, 2001);
 		Date expiryDate1 = new Date(8, 6, 2001);
@@ -280,40 +244,35 @@ public class Stock {
 		Date DIFF_productionDate = new Date(8, 5, 2001);
 		Date DIFF_expiryDate = new Date(8, 10, 2001);
 
-		FoodItem food1 = new FoodItem("Chreios", 1234, 1, DIFF_productionDate, DIFF_expiryDate, 0, 5, 25);
-		//System.out.println(food1.toString());
-		FoodItem food2 = new FoodItem("Chreios", 1234, 1, productionDate1, expiryDate1, 0, 20, 50);
-	//	System.out.println(food2.toString());
-		FoodItem food5 = new FoodItem("Oreo", 1234, 1, productionDate1, expiryDate1, 0, 20, 50);
+		FoodItem food1 = new FoodItem("Chreios", 1234, 4, DIFF_productionDate, DIFF_expiryDate, 0, 5, 25);
+		// System.out.println(food1.toString());
+		FoodItem food2 = new FoodItem("Chreios", 1234, 4, productionDate1, expiryDate1, 0, 20, 50);
+		// System.out.println(food2.toString());
+		FoodItem food5 = new FoodItem("Oreo", 1234, 5, productionDate1, expiryDate1, 0, 20, 50);
 
 		FoodItem food3 = new FoodItem("dog", 4321, 1, productionDate1, expiryDate1, 0, 20, 50);
-		//System.out.println(food3.toString());
+		// System.out.println(food3.toString());
 		FoodItem food4 = new FoodItem("cat", 4321, 1, productionDate1, expiryDate1, 0, 20, 900);
 
 		stock.addItem(food1);
-		//System.out.println(stock.getNumOfItems());
-		//System.out.println(food1.toString());
 
 		stock.addItem(food2);
-		//System.out.println(stock.getNumOfItems());
-		//System.out.println(food2.toString());
+
 		stock.addItem(food3);
-		//System.out.println(stock.getNumOfItems());
-		//System.out.println(food3.toString());
 
 		stock.addItem(food4);
-		//System.out.println(stock.getNumOfItems());
+
 		stock.addItem(food5);
-		//System.out.println(stock.getNumOfItems());
-/*
+
+		/*
+		 * System.out.println(stock.toString()); Date TodaysDate = new Date(8, 7, 2002);
+		 * //stock.getList(10); System.out.println(stock.toString());
+		 */
+
+		//System.out.println(stock.order(10));
+		String itemsList[]= {"Chreios","Chreios","Oreo","Oreo","Oreo"};
+		stock.updateStock(itemsList);
 		System.out.println(stock.toString());
-		Date TodaysDate = new Date(8, 7, 2002);
-		//stock.getList(10);
-		System.out.println(stock.toString());
-	*/
-		
-		System.out.println(stock.order(10));
-		
 		
 
 	}
